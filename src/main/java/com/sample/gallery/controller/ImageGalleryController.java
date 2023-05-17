48636f56f365863dc2eb57134294c6a05ddc8452
bereket honelign <bekhonelign@gmail.com>
2023-05-17 16:50:02 +0300

    cross origin

--- src/main/java/com/sample/gallery/controller/ImageGalleryController.java
+++ src/main/java/com/sample/gallery/controller/ImageGalleryController.java
@@ -17,9 +17,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
@@ -32,6 +34,8 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 @Controller
+@RequestMapping("/event")
+@CrossOrigin("http://localhost:8080/")
 public class ImageGalleryController {
 
     @Value("${uploadDir}")
