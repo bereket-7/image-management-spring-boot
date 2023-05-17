703f057d78cea4e7c1091f9ba3c109d5544735eb
bereket honelign <bekhonelign@gmail.com>
2023-05-17 13:14:51 +0300

    repository class

--- src/main/java/com/sample/gallery/repository/ImageGalleryRepository.java
+++ src/main/java/com/sample/gallery/repository/ImageGalleryRepository.java
@@ -2,6 +2,8 @@ package com.sample.gallery.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageGalleryRepository extends JpaRepository<ImageGallery,Integer>{
+import com.sample.gallery.entity.ImageGallery;
+
+public interface ImageGalleryRepository extends JpaRepository<ImageGallery, Long>{
     
 }
