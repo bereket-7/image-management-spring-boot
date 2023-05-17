fa44389809bc575d03240019519a9fb274167a3b
bereket honelign <bekhonelign@gmail.com>
2023-05-17 13:48:48 +0300

    fix pom file

--- src/main/java/com/sample/gallery/controller/ImageGalleryController.java
+++ src/main/java/com/sample/gallery/controller/ImageGalleryController.java
@@ -14,6 +14,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -40,7 +42,6 @@ public class ImageGalleryController {
 	public String addProductPage() {
 		return "index";
 	}
-    
 
     @PostMapping("/image/saveImageDetails")
 	public @ResponseBody ResponseEntity<?> createProduct(@RequestParam("name") String name,
